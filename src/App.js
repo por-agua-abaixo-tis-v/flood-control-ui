d62b355fab22495d6a7f73de86936769994c9ac2
Daniel Barros Pereira Sales <danielbps@outlook.com>
2020-03-04 22:37:36 -0300

    init(flood-control-ui): init front-end project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Flood-Control-Ui</header>
+    </div>
+  );
+}
+
+export default App;
