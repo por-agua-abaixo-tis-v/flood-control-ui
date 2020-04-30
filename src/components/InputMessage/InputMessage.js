c501d3c3d0c023f30ba5a3f5e5c31511afe87f36
Daniel Barros Pereira Sales <38352644+Danielbps@users.noreply.github.com>
2020-04-30 18:02:38 -0300

    Update InputMessage.js

--- src/components/InputMessage/InputMessage.js
+++ src/components/InputMessage/InputMessage.js
@@ -5,19 +5,21 @@ import axios from '../../axios-orders'
 const InputMessage = (props) => {
     const sendMethod = (event) => {
         event.preventDefault();
-        axios.post( 'https://tisv-flood-control-api.herokuapp.com/messages?group_id=' + props.groupId+'&user_id='+localStorage.getItem('id'),{"text":document.getElementById('input-message').value})
-        .then( response => {
-            document.getElementById('input-message').value = ''
-            this.messages = response.data;
-        } )
-        .catch( error => {
-        });
+        if (document.getElementById('input-message').value !== "") {
+            axios.post('https://tisv-flood-control-api.herokuapp.com/messages?group_id=' + props.groupId + '&user_id=' + localStorage.getItem('id'), { "text": document.getElementById('input-message').value })
+                .then(response => {
+                    document.getElementById('input-message').value = ''
+                    this.messages = response.data;
+                })
+                .catch(error => {
+                });
+        }
     }
-        return (
-            <form onSubmit={sendMethod} className={classes.inputForm}>
-                <input id='input-message' type="text" className={classes.input} placeholder="Sua mensagem.."></input>
-                <button className={classes.send}>Enviar</button>
-            </form>
-        )
+    return (
+        <form onSubmit={sendMethod} className={classes.inputForm}>
+            <input id='input-message' type="text" className={classes.input} placeholder="Sua mensagem.."></input>
+            <button disabled={!(document.getElementById('input-message') && !document.getElementById('input-message').value) === ""} className={classes.send}>Enviar</button>
+        </form>
+    )
 }
 export default InputMessage
